772ca4dfcc209d29e4b236ad0c6b0c7a353ed15a
Hasaan Qari <hassanqari9@gmail.com>
2024-10-31 16:08:23 +0530

    cors added

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,7 @@ import userRoutes from './routes/userRoutes.js'
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://vercel.com/safeautoparts/authentication-system-vercel/DiTrvpdTdqksFo6mPXm4SFoLsAQg', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
