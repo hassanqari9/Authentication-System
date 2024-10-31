a9417570155670dade2f6ba42c5519cd92f13115
Hasaan Qari <hassanqari9@gmail.com>
2024-10-31 15:56:00 +0530

    added new port in axios

--- client/src/apis/axios.js
+++ client/src/apis/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000',  
+    baseURL: 'https://authentication-system-qew1.onrender.com' || 'http://localhost:4000',  
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
